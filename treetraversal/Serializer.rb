2551d10b051bf3ff2fb5c2fe89a844bdef7e8e86
Luke Rogers <Roger4lk@dukes.jmu.edu>
2024-09-17 10:01:54 -0400

    good stuff still errors with constants

--- treetraversal/Serializer.rb
+++ treetraversal/Serializer.rb
@@ -1,79 +1,76 @@
-require_relative 'treetraversal/Visitor'
-require_relative 'treetraversal/AbstractSyntaxTree'
+require_relative 'Visitor'
+require_relative 'AbstractSyntaxTree'
 
 class Serializer include Visitor
   def visit(node)
-    case node.class
-    when AbstractSyntaxTree::IntPrim
+    case "#{node.class}"
+    when "#{AbstractSyntaxTree::IntPrim}"
       return "#{node.value}"
-    when AbstractSyntaxTree::FloatPrim
+    when "#{AbstractSyntaxTree::FloatPrim}"
       return "#{node.value}"
-    when AbstractSyntaxTree::BoolPrim
+    when "#{AbstractSyntaxTree::BoolPrim}"
       return "#{node.value}"
-    when AbstractSyntaxTree::StringPrim
+    when "#{AbstractSyntaxTree::StringPrim}"
       return "#{node.value}"
-    when AbstractSyntaxTree::CellAddr
+    when "#{AbstractSyntaxTree::CellAddr}"
       return "#{node.value}"
-    when AbstractSyntaxTree::Add
+    when "#{AbstractSyntaxTree::Add}"
       return "(#{self.visit(node.left)} + #{self.visit(node.right)})"
-    when AbstractSyntaxTree::Subtract
+    when "#{AbstractSyntaxTree::Subtract}"
       return "(#{self.visit(node.left)} - #{self.visit(node.right)})"
-    when AbstractSyntaxTree::Multiply
+    when "#{AbstractSyntaxTree::Multiply}"
       return "(#{self.visit(node.left)} * #{self.visit(node.right)})"
-    when AbstractSyntaxTree::Divide
+    when "#{AbstractSyntaxTree::Divide}"
       return "(#{self.visit(node.left)} / #{self.visit(node.right)})"
-    when AbstractSyntaxTree::Modulo
+    when "#{AbstractSyntaxTree::Modulo}"
       return "(#{self.visit(node.left)} % #{self.visit(node.right)})"
-    when AbstractSyntaxTree::AndLog
+    when "#{AbstractSyntaxTree::AndLog}"
       return "(#{self.visit(node.left)} && #{self.visit(node.right)})"
-    when AbstractSyntaxTree::OrLog
+    when "#{AbstractSyntaxTree::OrLog}"
       return "(#{self.visit(node.left)} || #{self.visit(node.right)})"
-    when AbstractSyntaxTree::NotLog
+    when "#{AbstractSyntaxTree::NotLog}"
       return "(!#{self.visit(node.left)})"
-    when AbstractSyntaxTree::LValue
+    when "#{AbstractSyntaxTree::LValue}"
       return "LVALUE: (#{self.visit(node.row)}, #{self.visit(node.collumn)})"
-    when AbstractSyntaxTree::RValue
+    when "#{AbstractSyntaxTree::RValue}"
       return "RVALUE: (#{self.visit(node.row)}, #{self.visit(node.collumn)})"
-    when AbstractSyntaxTree::AndBitw
+    when "#{AbstractSyntaxTree::AndBitw}"
       return "(#{self.visit(node.left)} & #{self.visit(node.right)})"
-    when AbstractSyntaxTree::OrBitw
+    when "#{AbstractSyntaxTree::OrBitw}"
       return "(#{self.visit(node.left)} | #{self.visit(node.right)})"
-    when AbstractSyntaxTree::RshiftBitw
+    when "#{AbstractSyntaxTree::RshiftBitw}"
       return "(#{self.visit(node.left)} >> #{self.visit(node.right)})"
-    when AbstractSyntaxTree::LshiftBitw
+    when "#{AbstractSyntaxTree::LshiftBitw}"
       return "(#{self.visit(node.left)} << #{self.visit(node.right)})"
-    when AbstractSyntaxTree::NotBitw
+    when "#{AbstractSyntaxTree::NotBitw}"
       return "(~#{self.visit(node.value)})"
-    when AbstractSyntaxTree::Equal
+    when "#{AbstractSyntaxTree::Equal}"
       return "(#{self.visit(node.left)} == #{self.visit(node.right)})"
-    when AbstractSyntaxTree::NotEqual
+    when "#{AbstractSyntaxTree::NotEqual}"
       return "(#{self.visit(node.left)} != #{self.visit(node.right)})"
-    when AbstractSyntaxTree::LessThan
+    when "#{AbstractSyntaxTree::LessThan}"
       return "(#{self.visit(node.left)} < #{self.visit(node.right)})"
-    when AbstractSyntaxTree::LessEqual
+    when "#{AbstractSyntaxTree::LessEqual}"
       return "(#{self.visit(node.left)} <= #{self.visit(node.right)})"
-    when AbstractSyntaxTree::GreaterThan
+    when "#{AbstractSyntaxTree::GreaterThan}"
       return "(#{self.visit(node.left)} > #{self.visit(node.right)})"
-    when AbstractSyntaxTree::GreaterEqual
+    when "#{AbstractSyntaxTree::GreaterEqual}"
       return "(#{self.visit(node.left)} >= #{self.visit(node.right)})"
-    when AbstractSyntaxTree::FloatToInt
+    when "#{AbstractSyntaxTree::FloatToInt}"
       return "(Int) #{self.visit(node.left)})"
-    when AbstractSyntaxTree::IntToFloat
+    when "#{AbstractSyntaxTree::IntToFloat}"
       return "(Float)#{self.visit(node.left)})"
-    when AbstractSyntaxTree::Max
+    when "#{AbstractSyntaxTree::Max}"
       return "MAX(#{self.visit(node.left)}, #{self.visit(node.right)})"
-    when AbstractSyntaxTree::Min
+    when "#{AbstractSyntaxTree::Min}"
       return "MIN(#{self.visit(node.left)}, #{self.visit(node.right)})"
-    when AbstractSyntaxTree::Mean
+    when "#{AbstractSyntaxTree::Mean}"
       return "AVERAGE(#{self.visit(node.left)}, #{self.visit(node.right)})"
-    when AbstractSyntaxTree::Sum
+    when "#{AbstractSyntaxTree::Sum}"
       return "SUM(#{self.visit(node.left)}, #{self.visit(node.right)})"
     else
       raise "Illegal Arguments: #{node.class} is not a valid arg type"
     end
   end
-
-  
 end
-product = AbstractSyntaxTree::Multiply.new(AbstractSyntaxTree::Add.new(AbstractSyntaxTree::IntPrim.new(2), AbstractSyntaxTree::IntPrim.new(4)), AbstractSyntaxTree::IntPrim.new(3))
-puts product.traverse(Serializer.new())
+
