2fc878ea43d2c90db7539e360ba0a9cb0a2bc55e
Roger4lk <163892071+Roger4lk@users.noreply.github.com>
2024-09-19 11:39:23 -0400

    progress on evaluator

--- treetraversal/Evaluator.rb
+++ treetraversal/Evaluator.rb
@@ -1,9 +1,165 @@
-class Evaluator 
-  def visitSetPrimitive(node)
-    return node
+require_relative 'AbstractSyntaxTree'
+require_relative 'Visitor'
+
+class Evaluator include Visitor
+  def visit(node, payload=nil)
+    case "#{node.class}"
+    when P_INT
+      return node
+    when P_FLT
+      return node
+    when P_BOOL
+      return node
+    when P_STRING
+      return node
+    when P_CELL_ADDR
+      return node
+    when A_ADD
+      
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_INT && "#{right.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim.new(left.value + right.value)
+      elsif "#{left.class}" == P_FLT && "#{right.class}" == P_FLT || "#{left.class}" == P_FLT && "#{right.class}" == P_INT || "#{left.class}" == P_INT && "#{right.class}" == P_FLT
+        return AbstractSyntaxTree::FloatPrim.new(left.value + right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim/FloatPrim, IntPrim/FloatPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when SUB
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_INT && "#{right.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim.new(left.value - right.value)
+      elsif "#{left.class}" == P_FLT && "#{right.class}" == P_FLT || "#{left.class}" == P_FLT && "#{right.class}" == P_INT || "#{left.class}" == P_INT && "#{right.class}" == P_FLT
+        return AbstractSyntaxTree::FloatPrim.new(left.value - right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim/FloatPrim, IntPrim/FloatPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when A_MULT
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_INT && "#{right.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim.new(left.value * right.value)
+      elsif "#{left.class}" == P_FLT && "#{right.class}" == P_FLT || "#{left.class}" == P_FLT && "#{right.class}" == P_INT || "#{left.class}" == P_INT && "#{right.class}" == P_FLT
+        return AbstractSyntaxTree::FloatPrim.new(left.value * right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim/FloatPrim, IntPrim/FloatPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when A_DIVIDE
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if ("#{left.class}" == P_INT || "#{left.class}" == P_FLT) && ("#{right.class}" == P_INT || "#{right.class}" == P_FLT)
+        return AbstractSyntaxTree::FloatPrim.new(left.value / right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim/FloatPrim, IntPrim/FloatPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when A_MOD
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{right.class}" == P_INT && "#{left.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim.new(left.value % right.value)
+      end
+      if ("#{left.class}" == P_INT || "#{left.class}" == P_FLT) && ("#{right.class}" == P_INT || "#{right.class}" == P_FLT)
+        return AbstractSyntaxTree::FloatPrim.new(left.value % right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim/FloatPrim, IntPrim/FloatPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when L_AND
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_BOOL || "#{left.class}" == P_BOOL
+        return AbstractSyntaxTree::BoolPrim.new(left.value && right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (BoolPrim, BoolPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when L_OR
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_BOOL || "#{left.class}" == P_BOOL
+        return AbstractSyntaxTree::BoolPrim.new(left.value || right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (BoolPrim, BoolPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when L_NOT
+      value = self.visit(node.value)
+      if "#{left.class}" == P_BOOL || "#{left.class}" == P_BOOL
+        return AbstractSyntaxTree::BoolPrim.new(!left.value)
+      else
+        raise IllegalArgumentError, "Expexted: (BoolPrim) \n Actual: (#{left.class})"
+      end
+    when LVAL
+      raise NotImplementedError
+    when RVAL
+      raise NotImplementedError
+    when B_AND
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_INT || "#{left.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim(left.value & right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim, IntPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when B_OR
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_INT || "#{left.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim(left.value | right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim, IntPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when B_RSHIFT
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_INT || "#{left.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim(left.value >> right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim, IntPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when B_LSHIFT
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      if "#{left.class}" == P_INT || "#{left.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim(left.value << right.value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim, IntPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when B_NOT
+      value = self.visit(node.left)
+      if "#{value.class}" == P_INT
+        return AbstractSyntaxTree::IntPrim(~value)
+      else
+        raise IllegalArgumentError, "Expexted: (IntPrim, IntPrim) \n Actual: (#{left.class}, #{right.class})"
+      end
+    when R_EQ
+      return "(#{self.visit(node.left)} == #{self.visit(node.right)})"
+    when R_NOT_EQ
+      return "(#{self.visit(node.left)} != #{self.visit(node.right)})"
+    when R_LESS
+      return "(#{self.visit(node.left)} < #{self.visit(node.right)})"
+    when R_LESS_EQ
+      return "(#{self.visit(node.left)} <= #{self.visit(node.right)})"
+    when R_GREATER
+      return "(#{self.visit(node.left)} > #{self.visit(node.right)})"
+    when R_GREATER_EQ
+      return "(#{self.visit(node.left)} >= #{self.visit(node.right)})"
+    when C_FLT_TO_INT
+      return "(Int) #{self.visit(node.left)})"
+    when C_INT_TO_FLT
+      return "(Float)#{self.visit(node.left)})"
+    when S_MAX
+      return "MAX(#{self.visit(node.left)}, #{self.visit(node.right)})"
+    when S_MIN
+      return "MIN(#{self.visit(node.left)}, #{self.visit(node.right)})"
+    when S_MEAN
+      return "AVERAGE(#{self.visit(node.left)}, #{self.visit(node.right)})"
+    when S_SUM
+      return "SUM(#{self.visit(node.left)}, #{self.visit(node.right)})"
+    else
+      raise "Illegal Arguments: #{node.class} is not a valid arg type"
+    end
   end
 
-  def visitIntersect(node)
-    visit the left and right nodes of node and evaluate
-  end
+  
 end
+
+puts AbstractSyntaxTree::Add.new(AbstractSyntaxTree::IntPrim.new(2), AbstractSyntaxTree::IntPrim.new(3)).traverse(Evaluator.new()).value
--- treetraversal/Serializer.rb
+++ treetraversal/Serializer.rb
@@ -1,76 +1,76 @@
-require_relative 'Visitor'
-require_relative 'AbstractSyntaxTree'
+require_relative './Visitor'
+require_relative './AbstractSyntaxTree'
 
 class Serializer include Visitor
-  def visit(node)
+  
+  def visit(node, payload = nil)
     case "#{node.class}"
-    when "#{AbstractSyntaxTree::IntPrim}"
+    when P_INT
       return "#{node.value}"
-    when "#{AbstractSyntaxTree::FloatPrim}"
+    when P_FLT
       return "#{node.value}"
-    when "#{AbstractSyntaxTree::BoolPrim}"
+    when P_BOOL
       return "#{node.value}"
-    when "#{AbstractSyntaxTree::StringPrim}"
+    when P_STRING
       return "#{node.value}"
-    when "#{AbstractSyntaxTree::CellAddr}"
+    when P_CELL_ADDR
       return "#{node.value}"
-    when "#{AbstractSyntaxTree::Add}"
+    when A_ADD
       return "(#{self.visit(node.left)} + #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::Subtract}"
+    when SUB
       return "(#{self.visit(node.left)} - #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::Multiply}"
+    when A_MULT
       return "(#{self.visit(node.left)} * #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::Divide}"
+    when A_DIVIDE
       return "(#{self.visit(node.left)} / #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::Modulo}"
+    when A_MOD
       return "(#{self.visit(node.left)} % #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::AndLog}"
+    when L_AND
       return "(#{self.visit(node.left)} && #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::OrLog}"
+    when L_OR
       return "(#{self.visit(node.left)} || #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::NotLog}"
+    when L_NOT
       return "(!#{self.visit(node.left)})"
-    when "#{AbstractSyntaxTree::LValue}"
+    when LVAL
       return "LVALUE: (#{self.visit(node.row)}, #{self.visit(node.collumn)})"
-    when "#{AbstractSyntaxTree::RValue}"
+    when RVAL
       return "RVALUE: (#{self.visit(node.row)}, #{self.visit(node.collumn)})"
-    when "#{AbstractSyntaxTree::AndBitw}"
+    when B_AND
       return "(#{self.visit(node.left)} & #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::OrBitw}"
+    when B_OR
       return "(#{self.visit(node.left)} | #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::RshiftBitw}"
+    when B_RSHIFT
       return "(#{self.visit(node.left)} >> #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::LshiftBitw}"
+    when B_LSHIFT
       return "(#{self.visit(node.left)} << #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::NotBitw}"
+    when B_NOT
       return "(~#{self.visit(node.value)})"
-    when "#{AbstractSyntaxTree::Equal}"
+    when R_EQ
       return "(#{self.visit(node.left)} == #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::NotEqual}"
+    when R_NOT_EQ
       return "(#{self.visit(node.left)} != #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::LessThan}"
+    when R_LESS
       return "(#{self.visit(node.left)} < #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::LessEqual}"
+    when R_LESS_EQ
       return "(#{self.visit(node.left)} <= #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::GreaterThan}"
+    when R_GREATER
       return "(#{self.visit(node.left)} > #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::GreaterEqual}"
+    when R_GREATER_EQ
       return "(#{self.visit(node.left)} >= #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::FloatToInt}"
+    when C_FLT_TO_INT
       return "(Int) #{self.visit(node.left)})"
-    when "#{AbstractSyntaxTree::IntToFloat}"
+    when C_INT_TO_FLT
       return "(Float)#{self.visit(node.left)})"
-    when "#{AbstractSyntaxTree::Max}"
+    when S_MAX
       return "MAX(#{self.visit(node.left)}, #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::Min}"
+    when S_MIN
       return "MIN(#{self.visit(node.left)}, #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::Mean}"
+    when S_MEAN
       return "AVERAGE(#{self.visit(node.left)}, #{self.visit(node.right)})"
-    when "#{AbstractSyntaxTree::Sum}"
+    when S_SUM
       return "SUM(#{self.visit(node.left)}, #{self.visit(node.right)})"
     else
       raise "Illegal Arguments: #{node.class} is not a valid arg type"
     end
   end
 end
-
